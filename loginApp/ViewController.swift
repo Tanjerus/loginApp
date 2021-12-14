78e3156db8a31eeeb99e8c0cc55e1fbaef27b19c
Tanjerus <Merserus@gmail.com>
2021-12-14 16:41:45 +0300

    interface v1

--- loginApp/ViewController.swift
+++ loginApp/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var userNameField: UITextField!
+    @IBOutlet var passwordField: UITextField!
+    @IBOutlet var logInButton: UIButton!
+    @IBOutlet var forgotUserNameButton: UIButton!
+    @IBOutlet var forgotPasswordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+    }
 
-
+    
+  //  @IBAction override func unwind(for segue: UIStoryboardSegue) {
+        
 }
 
---
+++ loginApp/WelcomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeViewController.swift
+//  loginApp
+//
+//  Created by Игорь Шевырёв on 14.12.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
